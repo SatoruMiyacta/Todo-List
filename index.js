9612bcc159dad2bc4908cbc11ff673630ae7f5e0
SatoruMiyacta <89257064+SatoruMiyacta@users.noreply.github.com>
2022-06-27 19:10:01 +0900

    編集不可

--- index.js
+++ index.js
@@ -13,11 +13,11 @@
 //     // 
 //     const listItem = document.createElement('tr');
 //     const showItem = addList.appendChild(listItem);
-//     showItem.innerHTMl = task;
+//     showItem.innerHTML = task;
 
 //     // 
 //     const deleteButton = document.createElement('button');
-//     deleteButton.innerHTMl = 'Delete';
+//     deleteButton.innerHTML = 'Delete';
 //     listItem.appendChild(deleteButton);
 
 //     // 
@@ -98,11 +98,10 @@
 
 // -----------3------------------
 
-const table = document.querySelector('table');
-const input = document.getElementById('input');
-const addBtn = document.getElementsByTagName('button')[0];
-const buttonEdit = document.getElementsByTagName('button')[1];
-const buttonDelete = document.getElementsByTagName('button')[2];
+const table = document.querySelector('.table-list');
+const input = document.querySelector('.form');
+const addBtn = document.querySelector('.c-btn');
+
 
 // 配列
 
@@ -150,7 +149,7 @@ const createListView = () => {
 
     // table作成
 
-    todos.forEach((todo) => {
+    todos.forEach((todo,index) => {
 
     const todoItem = document.createElement("tr");
     const todoId = document.createElement("td");
@@ -159,32 +158,36 @@ const createListView = () => {
     const editBtn = document.createElement("button");
     const todoDelete = document.createElement("td");
     const deleteBtn = document.createElement("button");
+    const saveBtn = document.createElement("button");
 
     todoId.textContent = todo.id;
     todoTitle.textContent = todo.title;
     editBtn.textContent = "Edit";
     editBtn.classList.add("c-btn");
-    todoEdit.appendChild(editBtn);
     deleteBtn.textContent = "Delete";
     deleteBtn.classList.add("c-btn");
     deleteBtn.classList.add("secondary");
-    todoDelete.appendChild(deleteBtn);
+    
 
+    todoEdit.appendChild(editBtn);
+    todoDelete.appendChild(deleteBtn);
     todoItem.appendChild(todoId);
     todoItem.appendChild(todoTitle);
     todoItem.appendChild(todoEdit);
     todoItem.appendChild(todoDelete);
-
     table.appendChild(todoItem);
 
-    // editBtn.addEventListener("click" , evt => {
-    //     evt.preventDefault();
-    //     editTasks(editBtn);
-    // });
+    editBtn.addEventListener("click" , evt => {
+        todoTitle.innerHTML = '<input class="change"/>';
+        const inputChange = document.querySelector('change');
+        console.log(inputChange);
+
+    });
 
     deleteBtn.addEventListener("click" , evt => {
         evt.preventDefault();
         deleteTasks(deleteBtn);
+        todos.splice(index,1);
     });
 
 
@@ -199,11 +202,6 @@ const deleteTasks = (deleteBtn) => {
 };
 
 
-
-// const editTasks = (editBtn) => {
-//     const chosenTask = deleteBtn.closest("tr");
-//     table.removeChild(chosenTask);
-// };
 
 
 
